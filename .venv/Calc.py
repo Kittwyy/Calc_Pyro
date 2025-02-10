96114da3ba0c46489fa86decf6f45ed728ed5512
Kittwiii <jadennischan13@gmail.com>
2025-02-10 10:02:00 +0100

    Changed Error Message.

--- .venv/Calc.py
+++ .venv/Calc.py
@@ -14,7 +14,7 @@ def calculate():
             entry.insert(0, str(result))
         except:
             entry.delete(0, tk.END)
-            entry.insert(0, "You are Stupid, you broke Calculator")
+            entry.insert(0, "Error!")
 
 def clear():
     entry.delete(0, tk.END)
